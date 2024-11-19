104999ee2196eb611d072ffbbee789a7fb47471a
bidulki <roseehgus@gmail.com>
2024-11-19 10:33:15 +0900

    update embedding

--- embedding.py
+++ embedding.py
@@ -1,15 +1,17 @@
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+import json
 
-embedding_model = OpenAIEmbeddings(model="text-embedding-ada-002")
+def embedding_title():
+    embedding_model = OpenAIEmbeddings(model="text-embedding-ada-002")
+    
+    with open("./document.json", 'r') as f:
+        document_json = json.load(f)
+        
+    documents = list(document_json.keys())
 
-documents = ["오타니 쇼헤이", "손흥민", "박지성", "크리스티아누 호날두", "켄 톰프슨", "데니스 리치",
-             "크리스토퍼 콜럼버스", "니콜라 테슬라", "토마스 에디슨", "한강 (작가)", "삼성전자",
-             "LG전자", "성균관대학교", "서울대학교", "다이제", "찰스 다윈", "마하트마 간디",
-             "조지 워싱턴", "마틴 루터 킹", "SK 하이닉스"]
+    faiss_index = FAISS.from_texts(documents, embedding_model)
+    faiss_index.save_local("./DB/faiss_index")
 
-embeddings = [embedding_model.embed_query(doc) for doc in documents]
-
-faiss_index = FAISS.from_texts(documents, embedding_model)
-
-faiss_index.save_local("./DB/faiss_index")
+if __name__=='__main__':
+    embedding_title()
